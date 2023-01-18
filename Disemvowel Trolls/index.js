9ba65df686a0f02407cac5cd4095a04b46bec207
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-18 23:02:09 +0300

    Added solution

--- Disemvowel Trolls/index.js
+++ Disemvowel Trolls/index.js
@@ -1,3 +1,14 @@
+// Split the input string and use the loop to find if any of the letters in the string exist in the list of vowels.
 function disemvowel(str){
-    return str;
+    let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    let newArr = []
+    let splitStr = str.split("")
+    for(let i = 0; i < splitStr.length; i++){
+        if(vowels.indexOf(splitStr[i]) === -1){
+            newArr.push(splitStr[i])
+        }
+    }
+    return newArr.join("");
 }
+
+ console.log(disemvowel("This website is for losers LOL!"))
