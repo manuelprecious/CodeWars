aa9d5b5aaed6758322b6a6af09502f09c4720dd4
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-19 00:01:28 +0300

    Added instructions for decoding the Morse Algorithm

--- Moving Zeros To The End/index.js
+++ Moving Zeros To The End/index.js
@@ -1,3 +1,16 @@
 function moveZeros(arr) {
-    return
+  let newArr = [];
+  let zerosArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zerosArr.push(arr[i]);
+    } else {
+      newArr.push(arr[i]);
+    }
   }
+  return newArr.concat(zerosArr);
+  //    return [...newArr, ...zerosArr];
+}
+
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
